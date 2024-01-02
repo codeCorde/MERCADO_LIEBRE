fb5c6646434082195121c49a2ef807859b0301c7
codeCorde <martinezfranlam@gmail.com>
2024-01-02 15:09:47 -0300

    rehice el home.html y añadí detalles en app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 app.get("/",(req,res) =>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
-})
+});
 
 
-app.listen(port, () => console.log(`http://localhost:${port}`))
+app.listen(port, () => console.log(`http://localhost:${port}`));
